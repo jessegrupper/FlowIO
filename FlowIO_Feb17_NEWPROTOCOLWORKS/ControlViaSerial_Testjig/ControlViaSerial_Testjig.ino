0a7b6c17ec3c1c690076e32db906500370152748
shtarbanov <alims@media.mit.edu>
2020-02-17 18:13:26 -0500

    Testjig updates

--- FlowIO_Feb17_NEWPROTOCOLWORKS/ControlViaSerial_Testjig/ControlViaSerial_Testjig.ino
+++ FlowIO_Feb17_NEWPROTOCOLWORKS/ControlViaSerial_Testjig/ControlViaSerial_Testjig.ino
@@ -7,11 +7,19 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    val = Serial.read();
-    //flowio.startInflation(val);
-    flowio.sendCommand('+', val);
-    Serial.println(val,BIN);
-    //flowio.setPorts(val);
+//  if(Serial.available() > 0){
+//    val = Serial.read();
+//    //flowio.startInflation(val);
+//    flowio.sendCommand('+', val);
+//    Serial.println(val,BIN);
+//    //flowio.setPorts(val);
+//  }
+
+  if(Serial.available() >= 2){ 
+    char actionChar = Serial.read(); //read the 1st char
+    char portsChar  = Serial.read();         //read the 2nd char
+    flowio.sendCommand(actionChar, portsChar);
+    Serial.flush();
+    while(Serial.available()>0) Serial.read();
   }
 }
