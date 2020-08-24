523ed676f17c86076d02dc4d8fd5665598d282ff
shtarbanov <alims@media.mit.edu>
2020-08-23 21:40:59 -0400

    Dual Release Implemented

--- FlowIO_Aug_23_DualRelease/FlowIO_LIBRARY/FlowIO_Pneumatics.cpp
+++ FlowIO_Aug_23_DualRelease/FlowIO_LIBRARY/FlowIO_Pneumatics.cpp
@@ -17,14 +17,14 @@
 	  	stopAction(ports);
 	  	if(_config == INFLATION_PARALLEL || _config == INFLATION_SERIES){
 		  	if(ports<<3 != 0){
-		  		openInletValve(); 
+		  		openInletValve();
 		  		setPorts(ports);
 		  		startPump(1, pwmValue);
 		  	}
 	  	}
 	}
-	void FlowIO::startVacuum(uint8_t ports, uint8_t pwmValue){ 
-		stopAction(ports); 
+	void FlowIO::startVacuum(uint8_t ports, uint8_t pwmValue){
+		stopAction(ports);
 		if(_config == GENERAL || _config == VACUUM_PARALLEL || _config == VACUUM_SERIES){
 			if(ports<<3 != 0){
 				openOutletValve();
@@ -37,35 +37,20 @@
 	void FlowIO::startVacuumHalfCapacity(uint8_t ports, uint8_t pwmValue){ //Only applicable for Vacuum_Series and Vacuum_Parallel configurations.
 		stopAction(ports);
 		if(_config == VACUUM_SERIES || _config == VACUUM_PARALLEL){
-			if(ports<<3 != 0){	
+			if(ports<<3 != 0){
 				openOutletValve();
-				setPorts(ports);  
+				setPorts(ports);
 				startPump(1, pwmValue);
 			}
 		}
 	}
 	void FlowIO::startRelease(uint8_t ports){
-		stopAction(ports);		
-		if(ports<<3 != 0){		
+		stopAction(ports);
+		if(ports<<3 != 0){
 			setPorts(ports);
-			if(_config == INFLATION_PARALLEL || _config == INFLATION_SERIES)
-				openOutletValve();  //In the inflation configurations, the outlet valve is the pressure release valve as it's not connected.
-			else if(_config == VACUUM_PARALLEL || _config == VACUUM_SERIES){
-				openInletValve();	//In the vacuum configurations, the inlet valve is the pressure release valve as it's not connected.
-			}
-			else if(_config == GENERAL){ //Determine which valve to open based on the pressure value.
-				if(getHardwareStateOf(SENSOR)==true){
-					float p = _getPressurePSI();
-					delay(10); //leave some time for the valve to open.
-					float pAtmospheric = 14.53;
-					if(p>=pAtmospheric) openOutletValve();
-					else if(p<pAtmospheric) openInletValve();
-					//TODO: Check if this also works OK for cases where we exceed the max pressure
-					//and for cases where the sensor is activated but a pressure is not read fine.
-				}
-				//If the pressure sensor has not been activated, then open the outlet valve.
-				else openOutletValve();
-			}
+			//We must open both valves. The outlet valve releases high pressure, the inlet valve releases low pressure.
+			openOutletValve();
+			openInletValve();
 		}
 	}
 	void FlowIO::stopAction(uint8_t ports){
@@ -76,4 +61,4 @@
 		closePorts(ports); //this only closes the ports indicated with bit value 1.
 	  	stopPump(1);
 		stopPump(2);
-	}	
+	}
